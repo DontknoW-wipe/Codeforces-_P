d9deab570f11a055e683482cba26644c70e25685
DontknoW-wipe <57296632+DontknoW-wipe@users.noreply.github.com>
2021-04-23 22:52:59 +0530

    Create A - Sum of 2050.cpp

---
+++ Contest 2050 and Codeforces Round #718 (Div. 1 + Div. 2)/A - Sum of 2050.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll unsigned long long int
+void solve()
+{
+	ll n,m;
+	cin>>n>>m;
+	ll b[n][m];
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			cin>>b[i][j];
+		}
+	}
+	
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
